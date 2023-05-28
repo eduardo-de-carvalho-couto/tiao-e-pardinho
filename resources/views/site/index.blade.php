5f863040ff7b9ac25970248564aa15f2e790d719
Eduardo <coutoeduz@gmail.com>
2023-05-28 15:30:14 -0300

    Criando as migrations

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,172 @@
-<img src="{{ asset('storage/assets/background.png') }}" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tião e Pardinho</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+</head>
+<body>
+    <div class="container">
+        <div class="container__centralizador">
+            <div class="header">
+                <img src="{{ asset('storage/assets/logo.png') }}" class="header__logo">
+                <h2 class="header__discografia">Discografia</h2>
+            </div>
+            <div class="pesquisar">
+                <form action="">
+                    <label for="palavra" class="pesquisar__label">Digite uma palavra chave:</label>
+                    <div class="pesquisar__inputEbotao">
+                        <input type="text" id="palavra" class="pesquisar__input">
+                        <button class="pesquisar__botao">Pesquisar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="albunsEfaixas">
+                <h4 class="albunsEfaixas__titulo">Álbum: Rei do Gado, 1961</h4>
+                <ul class="faixas">
+                    <li class="faixas__header">
+                        <ul class="faixas__header--numeroEfaixa">
+                            <li>N°</li>
+                            <li>Faixa</li>
+                        </ul>
+                        <ul class="faixas__header--duracao">
+                            <li>Duração</li>
+                        </ul>
+                    </li>
+
+
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="albunsEfaixas">
+                <h4 class="albunsEfaixas__titulo">Álbum: Rei do Gado, 1961</h4>
+                <ul class="faixas">
+                    <li class="faixas__header">
+                        <ul class="faixas__header--numeroEfaixa">
+                            <li>N°</li>
+                            <li>Faixa</li>
+                        </ul>
+                        <ul class="faixas__header--duracao">
+                            <li>Duração</li>
+                        </ul>
+                    </li>
+
+
+                    <li class="faixas__opcao">
+                        <ul class="faixas__opcao--numeroEfaixa">
+                            <li>11</li>
+                            <li>Minas Gerais</li>
+                        </ul>
+                        <ul class="faixas__opcao--duracao">
+                            <li>3:47</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
